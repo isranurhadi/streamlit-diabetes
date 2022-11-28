d6a0d618223c25e6130c71af9f18f0a824f0d581
isranurhadi <108585441+isranurhadi@users.noreply.github.com>
2022-11-28 11:27:39 +0700

    Add files via upload

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -12,18 +12,18 @@ col1, col2 = st.columns(2)
 
 with col1 :
     Pregnancies = st.text_input('Input nilai Pregnancies')
-with col2 :
+with col1 :
     Glucose = st.text_input('Input nilai Glucose')
 with col1 :
     BloodPressure = st.text_input('Input nilai BloodPressure')
-with col2 :
+with col1 :
     SkinThickness = st.text_input('Input nilai SkinThickness')
 
-with col1 :
+with col2 :
     Insulin = st.text_input('Input nilai Insulin')
 with col2 :   
     BMI = st.text_input('Input nilai BMI')
-with col1 :
+with col2 :
     DiabetesPedigreeFunction = st.text_input('Input nilai DiabetesPedigreeFunction')
 with col2 :
     Age = st.text_input('Input nilai Age')
